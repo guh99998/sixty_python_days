369e0bfd74cd0a27b4dff8549cdc1ecdc8c0d15b
Gustavo Lopes <guhrlopes999@gmail.com>
2025-02-18 21:38:18 -0300

    feat: add the second day code
    An other way to sum two numbers

---
+++ Day 2/def_two_numbers_sum.py
@@ -0,0 +1,8 @@
+def two_numbers_sum():
+    number_1 = int(input('Input the first number: '))
+    number_2 = int(input('Input the second number: '))
+    sum_numbers = number_1 + number_2
+    print(sum_numbers)
+    return sum_numbers
+
+two_numbers_sum()
