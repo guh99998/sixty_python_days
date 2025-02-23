e7386cd3291d1715acc54e3e7075a100271cc440
Gustavo Lopes <guhrlopes999@gmail.com>
2025-02-23 20:17:20 -0300

    feat: add the another version of seventh day code

---
+++ Day 7/funcional_dictionary.py
@@ -0,0 +1,8 @@
+person = {}
+
+person['name'] = input('Input your name: ')
+person['course'] = input('Input your course: ')
+person['id'] = int(input('Input your course id: '))
+person['age'] = int(input('Input your age: '))
+
+print(person)
