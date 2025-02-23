7f4152c4f5d32c377e6b9a25718f829c998dd004
Gustavo Lopes <guhrlopes999@gmail.com>
2025-02-23 20:16:40 -0300

    feat: add the seventh day code

---
+++ Day 7/dictionary.py
@@ -0,0 +1,11 @@
+person = {
+    'name': 'Gustavo',
+    'last-name': 'Lopes',
+    'age': 22,
+    'course': 'Python Hacking',
+    'id': '99316'
+}
+
+print('Name:', person['name'])
+print('Age:', person['age'])
+print('Course', person['course'])
