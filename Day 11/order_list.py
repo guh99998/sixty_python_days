4af116c637f34e3ebaa3dde3911b054d29c0b1b3
Gustavo Lopes <guhrlopes999@gmail.com>
2025-02-24 17:16:05 -0300

    feat: add the eleventh day code

---
+++ Day 11/order_list.py
@@ -0,0 +1,9 @@
+num_list = [1, 2056, 6531650, 0, 350, 10, 510, 99, 63, 52, 4, 23, 56, 6]
+
+ordered_nums = sorted(num_list)
+
+print(ordered_nums)
+
+ordered_nums_2 = sorted(num_list, reverse=True)
+
+print(ordered_nums_2)
